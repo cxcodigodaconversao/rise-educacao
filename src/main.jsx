2af515164b317d97dc14658b17dbde443e0e81a1
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-09-16 16:15:23 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ConvertaVoxApp from './ConvertaVoxApp.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConvertaVoxApp />
+  </React.StrictMode>,
+)
